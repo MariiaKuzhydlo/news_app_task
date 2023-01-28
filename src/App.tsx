f7ae0b1d3f600f36bcbb2496565cb666ef93f9da
Mariia Kuzhydlo <86382728+MariiaKuzhydlo@users.noreply.github.com>
2023-01-28 14:18:47 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
-// import NewsFullInfo from "./components/NewsFullInfo/NewsFullInfo";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewsFullInfo from "./components/NewsFullInfo/NewsFullInfo";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
    return (
-      <Router>
-         <div className="App">
-            <Routes>
-               <Route path="/" element={<HomePage />} />
-               {/* <Route path="/:id" element={<NewsFullInfo />} /> */}
-            </Routes>
-         </div>
-      </Router>
+      // <HashRouter>
+      <div className="App">
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/:id" element={<NewsFullInfo />} /> */}
+         </Routes>
+      </div>
+      // {/* </HashRouter> */}
    );
 }
 
