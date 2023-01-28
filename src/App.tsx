50d67822a794d8a29b70bb090a3b88eeded2bb02
Mariia Kuzhydlo <86382728+MariiaKuzhydlo@users.noreply.github.com>
2023-01-28 13:58:55 +0200

    update BrouserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 // import NewsFullInfo from "./components/NewsFullInfo/NewsFullInfo";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
    return (
-      <HashRouter>
+      <Router>
          <div className="App">
             <Routes>
                <Route path="/" element={<HomePage />} />
                {/* <Route path="/:id" element={<NewsFullInfo />} /> */}
             </Routes>
          </div>
-      </HashRouter>
+      </Router>
    );
 }
 
